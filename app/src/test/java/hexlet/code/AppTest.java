da85bb0f2b21518f9a6a17334f76e2a9f3bcad55
JamaL <jamalikowow@mail.ru>
2023-08-26 19:29:43 +0300

    1

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testStringMain () {
+        String mainString  = "Here Im";
+        assertEquals ("Here Im", mainString);
+    }
+}
