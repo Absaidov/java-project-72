3b8043791c28a80d861ff384d5953f476bd7e977
JamaL <jamalikowow@mail.ru>
2023-08-26 20:52:59 +0300

    jama

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,12 +1,11 @@
 package hexlet.code;
 
-import io.ebeaninternal.server.util.Str;
+//import io.ebeaninternal.server.util.Str;
 import org.junit.jupiter.api.Test;
 
 import static hexlet.code.App.helloString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import hexlet.code.App;
 
 public class AppTest {
 
