9b23435cc236c6e456ceab853375972533c06e6a
JamaL <jamalikowow@mail.ru>
2023-08-27 17:26:04 +0300

    ending

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,29 +1,31 @@
 package hexlet.code;
 
+import hexlet.code.controllers.RootController;
+import hexlet.code.controllers.UrlController;
 import io.javalin.Javalin;
-//import io.javalin.plugin.rendering.template.JavalinThymeleaf;
-//import io.javalin.rendering.template.JavalinThymeleaf;
-//import static io.javalin.apibuilder.ApiBuilder.path;
-//import static io.javalin.apibuilder.ApiBuilder.get;
-//import static io.javalin.apibuilder.ApiBuilder.post;
-//
-//import org.thymeleaf.TemplateEngine;
-//import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-//import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
-//import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+import static io.javalin.apibuilder.ApiBuilder.path;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.post;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
 
 
-public class App {
+public final class App {
+
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
             if (!isProduction()) {
                 config.plugins.enableDevLogging();
             }
-//            config.enableWebjars();
-//            JavalinThymeleaf.configure(getTemplateEngine());
+            config.enableWebjars();
+            JavalinThymeleaf.configure(getTemplateEngine());
         });
 
-//        addRoutes(app);
+        addRoutes(app);
 
         app.before(ctx -> {
             ctx.attribute("ctx", ctx);
@@ -34,12 +36,12 @@ public class App {
 
     public static void main(String[] args) {
         Javalin app = getApp();
-//        app.start(getPort());
-//        Javalin.create(config -> {
-//            config.plugins.enableDevLogging();
-//        });
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070);
+        app.start(getPort());
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        return Integer.valueOf(port);
     }
 
     private static String getMode() {
@@ -50,18 +52,32 @@ public class App {
         return getMode().equals("production");
     }
 
-//    private static TemplateEngine getTemplateEngine() {
-//        TemplateEngine templateEngine = new TemplateEngine();
-//
-//        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-//        templateResolver.setPrefix("/templates/");
-//
-//        templateResolver.setCharacterEncoding("UTF-8");
-//
-//        templateEngine.addTemplateResolver(templateResolver);
-//        templateEngine.addDialect(new LayoutDialect());
-//        templateEngine.addDialect(new Java8TimeDialect());
-//
-//        return templateEngine;
-//    }
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", RootController.welcome);
+        app.routes(() -> {
+            path("urls", () -> {
+                get(UrlController.listURLs);
+                post(UrlController.createUrl);
+                path("{id}", () -> {
+                    get(UrlController.showUrl);
+                    post("/checks", UrlController.checkUrl);
+                });
+            });
+        });
+    }
 }
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,18 +1,183 @@
 package hexlet.code;
 
-//import org.junit.jupiter.api.Test;
 
-//import static hexlet.code.App.helloString;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
+import hexlet.code.domain.Url;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import io.javalin.Javalin;
+import io.ebean.DB;
+import hexlet.code.domain.query.QUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import io.ebean.Transaction;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class AppTest {
+    @Test
+    void testInit() {
+        assertThat(true).isEqualTo(true);
+    }
 
-//    @Test
-//    public void testStringMain() {
-//        assertEquals(helloStringTest(), helloString());
-//    }
-//    public static String helloStringTest() {
-//        return "Here Im";
-//    }
+    private static Javalin app;
+    private static String baseUrl;
+    private static Url existingUrl;
+    private static Transaction transaction;
+    private static final String SAMPLE_DIRECTORY = "src/test/resources";
+
+    @BeforeAll
+    public static void beforeAll() throws IOException {
+        app = App.getApp();
+        app.start(0);
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+        existingUrl = new Url("https://youtube.com");
+        existingUrl.save();
+    }
+
+    @AfterAll
+    public static void afterAll() throws IOException {
+        app.stop();
+    }
+
+    @BeforeEach
+    final void beforeEach() {
+        transaction = DB.beginTransaction();
+    }
+
+    @AfterEach
+    final void afterEach() {
+        transaction.rollback();
+    }
+
+    @Test
+    void testIndex() {
+        HttpResponse<String> response = Unirest.get(baseUrl).asString();
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+    @Test
+    void testCreateExistingUrl() {
+        HttpResponse<String> responsePost1 = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", existingUrl.getName())
+                .asEmpty();
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl)
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+    @Test
+    void testIncorrectShowId() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls/100")
+                .asString();
+        assertThat(response.getStatus()).isEqualTo(404);
+    }
+
+    @Test
+    void testIndexUrls() {
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+        String body = response.getBody();
+        assertThat(body).contains(existingUrl.getName());
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+    @Test
+    void testCreateUrl() {
+        String inputName = "https://youtube.com";
+        HttpResponse<String> responsePost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", inputName)
+                .asEmpty();
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+
+        Url actualUrl = new QUrl()
+                .name.equalTo(inputName)
+                .findOne();
+
+        assertThat(actualUrl).isNotNull();
+        assertThat(actualUrl.getName()).isEqualTo(inputName);
+    }
+    @Test
+    void testCreateInvalidUrl() {
+        String inputUrl = "Qwerty";
+        HttpResponse responsePost = Unirest
+                .post(baseUrl + "/urls")
+                .field("url", inputUrl)
+                .asEmpty();
+
+        HttpResponse<String> response = Unirest
+                .get(baseUrl + "/urls")
+                .asString();
+        String body = response.getBody();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(body).doesNotContain(inputUrl);
+
+        Url actualUrl = new QUrl()
+                .name.equalTo(inputUrl)
+                .findOne();
+
+        assertThat(actualUrl).isNull();
+    }
+
+    @Test
+    void testCheckUrl() throws Exception {
+        String samplePage = Files.readString(Paths.get(SAMPLE_DIRECTORY, "sample.html"));
+
+        MockWebServer mockServer = new MockWebServer();
+        mockServer.enqueue(new MockResponse().setBody(samplePage));
+        mockServer.start();
+        String samplePageUrl = mockServer.url("/").toString();
+
+        HttpResponse response = Unirest
+                .post(baseUrl + "/urls/")
+                .field("url", samplePageUrl)
+                .asEmpty();
+
+        Url url = new QUrl()
+                .name.equalTo(samplePageUrl.substring(0, samplePageUrl.length() - 1))
+                .findOne();
+
+        HttpResponse response1 = Unirest
+                .post(baseUrl + "/urls/" + url.getId() + "/checks")
+                .asString();
+
+        assertThat(response1.getStatus()).isEqualTo(302);
+        assertThat(response1.getHeaders().getFirst("Location")).isEqualTo("/urls/" + url.getId());
+
+        String body = Unirest
+                .get(baseUrl + "/urls/" + url.getId())
+                .asString()
+                .getBody();
+
+        assertThat(body).contains("200");
+        assertThat(body).contains("Sample title");
+        assertThat(body).contains("Sample description");
+        assertThat(body).contains("Sample header");
+
+    }
 }
+
